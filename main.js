3efd2e5891d5e9b95041186dc947ceadadf730fb
catalina <catalina@macpro.local>
2019-08-05 10:56:00 -0400

    mongo

--- main.js
+++ main.js
@@ -5,6 +5,27 @@ const port = 3000
 // app.get('/hello', (req, res) => {
 //     res.send('Hello World!')
 // })
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
+
+
 var data = [
     {"name":"pikachu","type":"electric","number":25,"img": "http://www.serebii.net/pokemongo/pokemon/025.png"},
     {"name":"Bulbasaur","type":"grass","number":01,"img": "http://www.serebii.net/pokemongo/pokemon/001.png"},
